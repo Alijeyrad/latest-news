f9609517d0f4847363affbe47e5fcc19608274df
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-03-24 17:08:12 +0430

    main design completed
    waiting for JavaScript

--- news.js
+++ news.js
@@ -1,6 +1,37 @@
+// scroll To bottom
 document.getElementById('about').addEventListener('click', () => {
   window.scrollTo({
     top: window.innerHeight,
     behavior: 'smooth'
   })
-})
+});
+// scroll To top
+document.getElementById('arrow').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+});
+
+
+
+// w3 code
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+// Close the sidebar with the close button
+function w3_close() {
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
